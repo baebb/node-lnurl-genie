745fd5bea014815d3b782829b3b60cb03b9a9fa7
Ross <me@rossdyson.com>
2019-12-06 22:21:14 +0900

    landing page UI done

--- pages/api/lnurl-one.js
+++ pages/api/lnurl-one.js
@@ -3,8 +3,8 @@
 
 const metadataString = [
     [
-        "text/plain", // mime-type, "text/plain" is the only supported type for now, must always be present
-        "The genie asks for some sats to answer your question..." // actual metadata content
+        'text/plain', // mime-type, "text/plain" is the only supported type for now, must always be present
+        'The genie asks for some sats to answer your question...' // actual metadata content
     ]
 ];
 
@@ -13,7 +13,7 @@ const firstResponse = {
     maxSendable: 465 * 1000, // max amount LN SERVICE is willing to receive in mSat
     minSendable: 465 * 1000, // min amount LN SERVICE is willing to receive in mSat, can not be less than 1 or more than `maxSendable`
     metadata: metadataString, // metadata json which must be presented as raw string here, this is required to pass signature verification at a later step
-    tag: "payRequest" // type of LNURL
+    tag: 'payRequest' // type of LNURL
 };
 
 // const Endpoint = async (req, res) => {
@@ -25,7 +25,7 @@ export default async (req, res) => {
     } else {
         res.status(200).json({
             ...firstResponse,
-            callback: `${serverUrl}/api/lnurl-two`,
+            callback: `https://${serverUrl}/api/lnurl-two`,
         });
     }
 };
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,16 @@ import QRCode from 'qrcode.react';
 // UI Dependencies
 import {
     Col,
-    Row
+    Row,
+    Typography,
+    Button,
+    Icon
 } from 'antd';
 
 // Util Dependencies
 import { buildLNURL } from '../utils';
+
+const { Text } = Typography;
 
 export default class extends Component {
     static async getInitialProps({ req }) {
@@ -20,20 +25,60 @@ export default class extends Component {
         };
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            showLnurlHelp: false,
+        };
+    }
+
     render() {
-        const genieLnurl = buildLNURL(`${this.props.serverUrl}/api/lnurl-one`);
+        const { showLnurlHelp } = this.state;
+        const genieLnurl = buildLNURL(`https://${this.props.serverUrl}/api/lnurl-one`);
 
         return (
             <Row style={{ marginTop: 100 }}>
                 <Col xs={24} sm={{ span: 8, offset: 8 }} style={{ textAlign: 'center' }}>
-                    <h1>server URL: {this.props.serverUrl}</h1>
-                    <a href={`lightning:${genieLnurl}`}>
-                        <QRCode
-                            value={genieLnurl}
-                            size={128}
-                            renderAs="svg"
+                    <div style={{ marginBottom: 20 }}>
+                        <img
+                            src="/genie.jpeg"
+                            width={200}
                         />
-                    </a>
+                    </div>
+                    <div style={{ marginBottom: 20 }}>
+                        <a href={`lightning:${genieLnurl}`}>
+                            <QRCode
+                                value={genieLnurl}
+                                size={128}
+                                renderAs="svg"
+                            />
+                        </a>
+                    </div>
+                    <div style={{ marginBottom: 10 }}>
+                        <Text code>Scan LNURL to ask genie a question</Text>
+                    </div>
+                    {/*<p>{this.props.serverUrl}</p>*/}
+                    <div>
+                        <Button
+                            type="link"
+                            size="small"
+                            onClick={() => this.setState({ showLnurlHelp: !showLnurlHelp })}
+                            style={{ marginBottom: 6 }}
+                        >
+                            <small>
+                                Which wallets are LNURL-compatible?&nbsp;
+                                {showLnurlHelp ? <Icon type="caret-up" /> : <Icon type="caret-down" />}
+                            </small>
+                        </Button>
+                        {showLnurlHelp && (
+                            <div style={{ marginBottom: 14 }}>
+                                <small>
+                                    <div style={{ marginBottom: 4 }}><a rel="noopener noreferrer" target="_blank" href="https://lightning-wallet.com/">Bitcoin Lightning Wallet</a> (Android)</div>
+                                    <div style={{ marginBottom: 4 }}><a rel="noopener noreferrer" target="_blank" href="https://t.me/lntxbot">lntxbot</a> (iOS, Android & Web)</div>
+                                </small>
+                            </div>
+                        )}
+                    </div>
                 </Col>
             </Row>
         );
