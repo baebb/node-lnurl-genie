340a7ff021c4187110816f01946edd40c82fd3fb
Ross <me@rossdyson.com>
2019-12-07 14:38:17 +0900

    give the req to the component

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
     const server = express();
 
+    server.get('/answer', (req, res) => {
+        return app.render(req, res, '/answer', { req });
+    });
+
     server.all('*', (req, res) => {
-        return handle(req, res)
+        return handle(req, res);
     });
 
     server.listen(port, err => {
